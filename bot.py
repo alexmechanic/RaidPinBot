a2d3a7aaffafb9e7214526d16ae28fe75d28ea9d
Alexander <samik.mechanic@gmail.com>
2019-08-10 13:25:29 +0300

    изменение вебхуков с Flask на cherrypy
    Flask работать с Heroku отказался

--- bot.py
+++ bot.py
@@ -7,9 +7,17 @@
 #
 
 import telebot, re, os, time
+import cherrypy
 from telebot import types
 from logger import get_logger
-from flask import Flask, request
+
+BOT_USERNAME = 'raidpinbot'
+WEBHOOK_HOST = BOT_USERNAME + '.herokuapp.com'
+WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
+
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
 
 log = get_logger("bot")
 
@@ -21,21 +29,25 @@ try:
 except FileNotFoundError: # Heroku run
     TOKEN = os.environ['TOKEN']
 
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % TOKEN
+
 bot = telebot.TeleBot(TOKEN)
-server = Flask(__name__)
 
-BOT_USERNAME = "raidpinbot"
-
-@server.route('/' + TOKEN, methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://' + BOT_USERNAME + '.herokuapp.com/' + TOKEN)
-    return "!", 200
+# WebhookServer, process webhook calls
+class WebhookServer(object):
+    @cherrypy.expose
+    def index(self):
+        if 'content-length' in cherrypy.request.headers and \
+           'content-type' in cherrypy.request.headers and \
+           cherrypy.request.headers['content-type'] == 'application/json':
+            length = int(cherrypy.request.headers['content-length'])
+            json_string = cherrypy.request.body.read(length).decode("utf-8")
+            update = telebot.types.Update.de_json(json_string)
+            bot.process_new_updates([update])
+            return ''
+        else:
+            raise cherrypy.HTTPError(403)
 
 #
 # Bot should pin only raid messages (assume they should have reply_markup keyboard)
@@ -62,5 +74,27 @@ def check_raidmessage(m):
     else:
         log.error("Not a raid message, skipping")
 
-server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8443)))
+
+# Remove webhook, it fails sometimes the set if there is a previous webhook
+bot.remove_webhook()
+
+# Set webhook
+bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+                certificate=open(WEBHOOK_SSL_CERT, 'r'))
+
+# Disable CherryPy requests log
+access_log = cherrypy.log.access_log
+for handler in tuple(access_log.handlers):
+    access_log.removeHandler(handler)
+
+# Start cherrypy server
+cherrypy.config.update({
+    'server.socket_host'    : WEBHOOK_LISTEN,
+    'server.socket_port'    : WEBHOOK_PORT,
+    'server.ssl_module'     : 'builtin',
+    'server.ssl_certificate': WEBHOOK_SSL_CERT,
+    'server.ssl_private_key': WEBHOOK_SSL_PRIV
+})
+
+cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
 # bot.polling(none_stop=True, interval=0, timeout=20)
